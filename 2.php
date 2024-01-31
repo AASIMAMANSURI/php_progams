e9fb219f748c5643f53e26a2d5e35ecc7063530c
AASIMAMANSURI <136147114+AASIMAMANSURI@users.noreply.github.com>
2024-01-31 12:39:32 +0530

    var_dump() function

---
+++ 2.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    // var_dump() function
+    //The var_dump() function returns the data type and the value of the veriable
+     $var1 ="aasima";
+     $var2 = 18;
+     $var3 = [18,45,46,58];
+     $var4 = (18.58);
+     $var5 = (false);
+
+     var_dump($var1);
+     echo '<br>';
+     var_dump($var2);
+     echo '<br>';
+     var_dump($var3);
+     echo '<br>';
+     var_dump($var4);
+     echo '<br>';
+     var_dump($var5);
+     echo '<br>';
+     echo '<br>';
+
+
+     // we can also know the data type by putting the value in var_dump() function :
+
+     var_dump("hey ...!");
+
+     
+    ?>
+</body>
+</html>
