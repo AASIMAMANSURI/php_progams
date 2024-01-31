79640897c8a3ceb2e39d4a6f3f31bef1de8ce7d0
AASIMAMANSURI <136147114+AASIMAMANSURI@users.noreply.github.com>
2024-01-31 12:24:22 +0530

    Update 1.php

--- 1.php
+++ 1.php
@@ -12,10 +12,28 @@
 
     <?php
 
-    $var ="aasima";
-
+    $var ="aasima";  // $var is string
+    $var1 =1; // $var1 is integer 
     echo "My Name id $var .";
+    echo "<br>$var1";   
 
+
+    // assigning same value to multiple vaeriable
+
+    $a = $b = $c ="same value";
+
+    echo "<br> $a ";
+    echo "<br> $b" ;
+    echo "<br> $c ";
     ?>
 </body>
 </html>
+
+
+//output:-
+php variable
+My Name id aasima .
+1
+same value
+same value
+same value
