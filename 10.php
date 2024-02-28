7a029ebd8cec387d5891d5a5337d39ae0e3912a3
AASIMAMANSURI <136147114+AASIMAMANSURI@users.noreply.github.com>
2024-02-28 13:05:52 +0530

    10(A).php

---
+++ 10.php
@@ -0,0 +1,31 @@
+<!-- 10. Write a PHP code to take input from user for no of row and column and
+generate dynamic table. -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="10(A).php" method="POST">
+    
+        <h2> Displaying table</h2>
+
+        <label for="table"> Enter the number of rows:</label>
+        <input type="number" name="rows" id="rows" required >
+
+        <br>
+        <br>
+        
+        <label for="table"> Enter the number of columns:</label>
+        <input type="number" name="columns" id="columns" required>
+
+        <input type="submit">
+
+    </form>
+   
+
+</body>
+</html>  
